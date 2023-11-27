f185a37c0cc49e8f37fddb7ef8cec4878aba94fa
fahimar <fahimar14040@gmail.com>
2023-11-27 21:53:38 +0600

    editMode todo list commited.

--- class-1/todo_list-app/src/app/todos/page.tsx
+++ class-1/todo_list-app/src/app/todos/page.tsx
@@ -5,6 +5,32 @@ import { useState } from "react";
 export default function Todos() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([1, 2, 3]);
+  const [editMode, setEditMode] = useState(false);
+
+  if (editMode) {
+    // true
+    return (
+      <div className="flex flex-col items-center gap-8 pt-8 bg-violet-200 pb-32">
+        <div className="text-2xl">Edit Todo</div>
+        <div className="flex gap-4">
+          <div className="text-lg">Edit desc:</div>
+          <input
+            className="rounded-md shadpw-md text-lg"
+            type="text"
+            placeholder="Enter new desc"
+          />
+        </div>
+        <div className="flex gap-4">
+          <div className="text-lg">Edit completed:</div>
+          <input type="checkbox" />
+        </div>
+        <button className="text-xl shadow-md bg-blue-600 text-white hover:bg-blue-500 rounded-md px-3 py-1">
+          Submit
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center gap-8 pt-8 bg-violet-200 pb-32">
       <div className="text-2xl">Todo List Next</div>
@@ -23,19 +49,19 @@ export default function Todos() {
           Clear
         </button>
       </div>
-      <div className="">
+      <div className="w-5/6 flex flex-col gap-2">
         {todos.map((todo, index) => {
           return (
-            <div className="">
-              <div className="">
+            <div className="bg-violet-600 flex justify-between items-center p-2 rounded-lg shadow-md">
+              <div className="flex gap-2">
                 <input type="checkbox" />
-                <div className="">Write code</div>
+                <div className="text-lg text-white">Write code</div>
               </div>
               <div className="flex gap-2">
-                <button className="text-xl shadow-md bg-green-600 text-white hover:bg-blue-500 rounded-md px-3 py-1">
+                <button className="text-xl shadow-md bg-green-600 text-white hover:bg-blue-500 rounded-md px-1">
                   Edit
                 </button>
-                <button className="text-xl shadow-md bg-red-600 text-white hover:bg-blue-500 rounded-md px-3 py-1">
+                <button className="text-xl shadow-md bg-red-600 text-white hover:bg-blue-500 rounded-md px-1">
                   Delete
                 </button>
               </div>
